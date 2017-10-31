78a6a39d95727bf3378500031198331ccb63c372
Pradeep Kumar <kumpr19@ca.com>
2017-10-31 15:22:54 +0530

    Created CA API Design theme for Mineral UI

---
+++ rapido-web/src/modules/theme/ca-api-design-theme.jsx
@@ -0,0 +1,10 @@
+module.exports = {
+	Button_backgroundColor: '#09AEEF',
+	Button_color_text: '#ffffff',
+	Button_fontWeight: '100',
+	Button_borderColor: '#09AEEF',
+	Button_borderWidth: '2px',
+	Button_backgroundColor_active: '#09AEEF',
+	Button_backgroundColor_focus: '#09AEEF',
+	Button_backgroundColor_hover: '#09AEEF'
+};
