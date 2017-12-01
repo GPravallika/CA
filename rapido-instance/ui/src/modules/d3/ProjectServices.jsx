4f66d747c1e687d783061dc2bee697afcc3a2e7f
Pradeep Kumar <kumpr19@ca.com>
2017-12-01 19:48:56 +0530

    Vocabulary API integration

--- rapido-instance/ui/src/modules/d3/ProjectServices.jsx
+++ rapido-instance/ui/src/modules/d3/ProjectServices.jsx
@@ -25,6 +25,30 @@ const ProjectApiCall = {
     });
   },
 
+  addVocabularyToProject(vocabArray, projectId) {
+    var token  = sessionStorage.getItem("token");
+      return fetch(apiObj.endPoint + 'project/' + projectId + '/vocabulary', { 
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      },
+      body: JSON.stringify(vocabArray)
+    });
+  },
+
+  deleteVocabularyFromProject(vocab, projectId) {
+    var token  = sessionStorage.getItem("token");
+    
+    return fetch(apiObj.endPoint + 'project/' + projectId + '/vocabulary/' + vocab, { 
+      method: 'DELETE', 
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      }
+    });
+  },
+
   updateTeamToProject(team, projectId) {
     var token  = sessionStorage.getItem("token");
       return fetch(apiObj.endPoint + 'project/' + projectId + '/team/' + team.id, { 
